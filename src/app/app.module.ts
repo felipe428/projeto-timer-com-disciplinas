c85b54a5825d753d8da261727c477ead5db35750
felipe428 <felipe.silva428@fatec.sp.gov.br>
2021-09-06 16:18:58 -0300

    inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,31 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
+import { HomeComponent } from './home/home.component';
+import { TimerComponent } from './timer/timer.component';
+import { DisciplinasComponent } from './disciplinas/disciplinas.component';
+
+import { RouterModule } from '@angular/router';
+import { TimerService } from './timer.service';
+import { DisciplinasService } from './disciplinas.service';
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'timer', component: TimerComponent },
+      { path: 'disciplinas', component: DisciplinasComponent }
+    ])
+  ],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    TimerComponent,
+    DisciplinasComponent
+  ],
+  bootstrap: [AppComponent],
+  providers: [TimerService, DisciplinasService]
 })
-export class AppModule { }
+export class AppModule {}
